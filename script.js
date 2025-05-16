90d81cae1e27c3ed0189da3435ede3b26ce5d4fd
roope <roope.aalto88@gmail.com>
2025-05-17 00:01:56 +0300

    some tuning

--- script.js
+++ script.js
@@ -1,35 +1,87 @@
 const mainSection = document.createElement("section");
-;
+const logoDiv = document.createElement("div");
+logoDiv.id = "logoDiv";
 const body = document.body;
 const topNavigationBar = document.createElement("div");
-
+const mealDiv = document.createElement("div");
+mealDiv.id = "mealDiv";
+const shoppingCart = document.createElement("div");
+shoppingCart.id="shoppingCart";
 topNavigationBar.id="topNavigationBar";
-topNavigationBar.style.height="175px";
-topNavigationBar.style.width="700px";
-topNavigationBar.style.backgroundColor="red";
+
+logoDiv.innerText="McBurger";
+body.appendChild(logoDiv);
+
 
 const mealSpan = document.createElement("span");
+mealSpan.className="navigationSpans";
 mealSpan.innerText="Meals";
+
+mealSpan.onclick = revealMeals;
 const hamburgerSpan = document.createElement("span");
+hamburgerSpan.className="navigationSpans";
 hamburgerSpan.innerText="Hamburgers";
 const drinkSpan = document.createElement("span");
+drinkSpan.className="navigationSpans";
 drinkSpan.innerText="Drinks";
 const sideSpan = document.createElement("span");
+sideSpan.className="navigationSpans";
 sideSpan.innerText="Sides";
 
-body.style.height="100vh";
-body.style.width="100vw";
-body.style.display="flex";
-body.style.justifyContent="center";
-body.style.alignItems="center";
-body.style.flexDirection="row";
+topNavigationBar.appendChild(mealSpan);
+topNavigationBar.appendChild(hamburgerSpan);
+topNavigationBar.appendChild(drinkSpan);
+topNavigationBar.appendChild(sideSpan);
+
 
 mainSection.id="mainSection"
-mainSection.style.backgroundColor="black";
-mainSection.style.height="700px";
-mainSection.style.width="700px";
-mainSection.style.display = "flex";
-mainSection.style.flexDirection="column";
+
 
 body.appendChild(mainSection);
+body.appendChild(shoppingCart);
+mainSection.appendChild(logoDiv);
 mainSection.appendChild(topNavigationBar);
+mainSection.appendChild(mealDiv);
+
+
+const meals = [{name: "Big Meal", price: 18.95},{name: "Cheesy One Meal",price: 13.99}, {name: "Double Stack Meal", price: 20.99}, {name: "Hot Burger Meal", price: 13.25},{name: "Crunchy Meal", price: 17.25},{name: "Beefy Bun Meal",price: 19.99},{name: "Classic Combo", price: 14.25},{name:"Grill Box", price: 26.99},{name: "Juicy Deal",price: 9.95},{name: "Snack Pack", price:11.85}]
+
+meals.forEach(element => {
+    const mealsMotherDiv = document.createElement("div");
+    mealsMotherDiv.className="mealsMotherDiv";
+    const removeSpace = element.name.replaceAll(" ","");
+    mealsMotherDiv.id = removeSpace;
+    for(let i = 0; i< 3; i++){
+        const mealsChildDiv = document.createElement("div");
+        
+        if(i == 0){
+            mealsChildDiv.innerText = element.name;
+            mealsChildDiv.className="mealsChildDivName";
+            mealsMotherDiv.appendChild(mealsChildDiv);
+        } else if(i == 1){
+            mealsChildDiv.innerText = "$"+element.price;
+            mealsChildDiv.className = "mealsChildDivPrice";
+            mealsMotherDiv.appendChild(mealsChildDiv);
+        } else if(i == 2){
+            const btn = document.createElement("button");
+            btn.innerText = "Add to Basket";
+            btn.className="addtoBasketButton";
+            mealsChildDiv.className = "mealsChildDivBtn";
+            mealsChildDiv.appendChild(btn);
+            mealsMotherDiv.appendChild(mealsChildDiv);
+        }
+        
+        
+    }
+   mealDiv.appendChild(mealsMotherDiv);
+});
+function revealMeals(){
+    mealDiv.style.display="flex";
+    mealDiv.style.flexDirection="column";
+    mealDiv.style.justifyContent="space-around";
+    mealDiv.style.alignItems="center";
+}
+function hideAll(){
+    mealDiv.style.display="none";
+}
+
